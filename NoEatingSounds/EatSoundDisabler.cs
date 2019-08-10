a391c61267ff69bfa4230cfcf048b898a752d887
MrPurple6411 <mrpurple6411@gmail.com>
2019-08-10 12:37:26 -0600

    NoEatingSounds

---
+++ NoEatingSounds/EatSoundDisabler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Harmony;
+using UnityEngine;
+
+namespace MrPurple
+{
+
+
+    [HarmonyPatch(typeof(CraftData))]
+    [HarmonyPatch("GetUseEatSound")]
+    internal class SoundBreaker
+    {
+
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (CodeInstruction code in instructions)
+            {
+                if (code.opcode == OpCodes.Ldstr)
+                    yield return new CodeInstruction(OpCodes.Ldstr, string.Empty);
+                else
+                    yield return code;
+            }
+        }
+
+
+
+    }
+}
---
+++ NoEatingSounds/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Harmony;
+
+namespace MrPurple
+{
+	public class Entry
+	{
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("MrPurple6411.NoEatingSounds");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
