41ba1413880a07ab7b97e867e630129ded9c9eb6
MrPurple6411 <mrpurple6411@gmail.com>
2019-08-28 15:58:44 -0600

    Resource Overload

---
+++ ResourceOverload/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Harmony;
+
+namespace ResourceOverload
+{
+    public class Entry
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("MrPurple6411.ResourceOverload");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ ResourceOverload/IncreaseResourceSpawn.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Harmony;
+
+namespace ResourceOverload
+{
+    [HarmonyPatch(typeof(CellManager))]
+    [HarmonyPatch("GetPrefabForSlot")]
+    class IncreaseResourceSpawn
+    {
+
+        public static bool Prefix(CellManager __instance, IEntitySlot slot, ref EntitySlot.Filler __result)
+        {
+            int num = 0;
+            bool flag = __instance.spawner == null;
+            bool result;
+            if (flag)
+            {
+                __result = default(EntitySlot.Filler);
+                result = false;
+            }
+            else if (slot.IsCreatureSlot())
+            {
+                do
+                {
+                    num++;
+                    __result = __instance.spawner.GetPrefabForSlot(slot, true);
+                }
+                while (string.IsNullOrEmpty(__result.classId) && num < 10);
+                result = false;
+            }
+            else
+            {
+                do
+                {
+                    num++;
+                    __result = __instance.spawner.GetPrefabForSlot(slot, true);
+                }
+                while (string.IsNullOrEmpty(__result.classId) && num < 100);
+                result = false;
+            }
+            return result;
+        }
+    }
+}
