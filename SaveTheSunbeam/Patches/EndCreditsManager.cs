2c07f6547e192750cdeb8026ff8ab79b61c2694f
AlexejheroYTB <alexejhero@gmail.com>
2019-09-13 23:57:27 +0300

    Update EndCreditsManager.cs

--- SaveTheSunbeam/Patches/EndCreditsManager.cs
+++ SaveTheSunbeam/Patches/EndCreditsManager.cs
@@ -1,5 +1,5 @@
 ﻿using Harmony;
-using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace SaveTheSunbeam.Patches
@@ -11,7 +11,14 @@ namespace SaveTheSunbeam.Patches
         [HarmonyPrefix]
         static void Prefix(EndCreditsManager __instance)
         {
-            Console.WriteLine(__instance.creditsText.gameObject.GetComponentInChildren<Text>().text);
+            var texts = GameObject.FindObjectsOfType<Text>();
+            var names = texts[0];
+            var roles = texts[1];
+            var main = texts[2];
+
+            names.text = "\n\nMrPurple6411\nAlexejheroYTB\n\n\n" + names.text;
+            roles.text = "\n\nMod Developer\nMod Developer\n\n\n" + roles.text;
+            main.text = "<b>Alternate Sunbeam Event</b>\n\n\n\n\n\n" + main.text;
         }
     }
 }
