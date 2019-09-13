370d47afb7326d2ccbb85e27390b014f65632b72
AlexejheroYTB <alexejhero@gmail.com>
2019-09-13 21:56:05 +0300

    a

--- SaveTheSunbeam/Patches/uGUI_SunbeamCountdown.cs
+++ SaveTheSunbeam/Patches/uGUI_SunbeamCountdown.cs
@@ -1,4 +1,5 @@
 ﻿using Harmony;
+using System;
 using UnityEngine.UI;
 
 namespace SaveTheSunbeam.Patches
@@ -7,13 +8,28 @@ namespace SaveTheSunbeam.Patches
     [HarmonyPatch("UpdateInterface")]
     static class uGUI_SunbeamCountdown_UpdateInterface
     {
-        [HarmonyPostfix]
-        static void Postfix(uGUI_SunbeamCountdown __instance)
+        [HarmonyPrefix]
+        static bool Prefix(uGUI_SunbeamCountdown __instance)
         {
+            if (!StoryGoalCustomEventHandler.main) return false;
+            if (!StoryGoalCustomEventHandler.main.countdownActive)
+            {
+                typeof(uGUI_SunbeamCountdown).GetMethod("HideInterface").Invoke(__instance, new object[0]);
+                return false;
+            }
+            typeof(uGUI_SunbeamCountdown).GetMethod("ShowInterface").Invoke(__instance, new object[0]);
+
+            float time = StoryGoalCustomEventHandler.main.endTime - DayNightCycle.main.timePassedAsFloat;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+            string text = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+            __instance.countdownText.text = text;
+
             if (!StoryGoalCustomEventHandler.main.gunDisabled)
                 __instance.countdownHolder.GetComponentsInChildren<Image>().ForEach(i => i.sprite = Mod.redSprite); 
             else
                 __instance.countdownHolder.GetComponentsInChildren<Image>().ForEach(i => i.sprite = Mod.blueSprite); 
+
+            return false;
         }
     }
 }
