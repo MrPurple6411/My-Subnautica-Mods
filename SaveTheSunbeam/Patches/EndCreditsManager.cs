ac3012e2faab7907954fb94fea6f0840151a3bf4
AlexejheroYTB <alexejhero@gmail.com>
2019-09-14 17:04:10 +0300

    Update EndCreditsManager.cs

--- SaveTheSunbeam/Patches/EndCreditsManager.cs
+++ SaveTheSunbeam/Patches/EndCreditsManager.cs
@@ -1,4 +1,6 @@
 ﻿using Harmony;
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,9 +18,34 @@ namespace SaveTheSunbeam.Patches
             var roles = texts[1];
             var main = texts[2];
 
-            names.text = "\n\nMrPurple6411\nAlexejheroYTB\n\n\n" + names.text;
-            roles.text = "\n\nMod Developer\nMod Developer\n\n\n" + roles.text;
-            main.text = "<b>Alternate Sunbeam Event</b>\n\n\n\n\n\n" + main.text;
+            names.text = "\n\nMrPurple6411\nAlexejheroYTB\nNairda\nSeraphim\nJgirard1733\n\n\n" + names.text;
+            roles.text = "\n\nMod Developer\nMod Developer\nModeler\nTranscript\nVoice Actor\n\n\n" + roles.text;
+            main.text = "<b>Alternate Sunbeam Event</b>\n\n\n\n\n\n\n\n\n" + main.text;
+            __instance.scrollMaxValue += 200;
+            __instance.secondsUntilScrollComplete -= 5.5f;
+        }
+    }
+
+    [HarmonyPatch(typeof(EndCreditsManager))]
+    [HarmonyPatch("ReturnToMainMenu")]
+    static class EndCreditsManager_ReturnToMainMenu
+    {
+        [HarmonyPostfix]
+        static IEnumerator Postfix(IEnumerator iterator, EndCreditsManager __instance)
+        {
+            bool found = false;
+            while (iterator.MoveNext())
+            {
+                if (iterator.Current is WaitForSeconds && !found)
+                {
+                    found = true;
+                    yield return new WaitForSeconds(__instance.secondsUntilScrollComplete - 3f + 5.5f);
+                }
+                else
+                {
+                    yield return iterator.Current;
+                }
+            }
         }
     }
 }
