c86242bb336717229c06448b8f4803ce6fcf22ad
MrPurple6411 <mrpurple6411@gmail.com>
2019-12-22 02:03:51 -0700

    Leviathans Never Flee 1.0.0

---
+++ LeviathansNeverFlee/Main.cs
@@ -0,0 +1,37 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace LeviathansNeverFlee
+{
+    public class Main
+    {
+        public static void Load()
+        {
+            try
+            {
+                HarmonyInstance.Create("MrPurple6411.LeviathansNeverFlee").PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(FleeOnDamage))]
+    [HarmonyPatch(nameof(FleeOnDamage.OnTakeDamage))]
+    internal class FleeOnDamage_StartPerform_Patch
+    {
+        [HarmonyPrefix]
+        static void Prefix(FleeOnDamage __instance, DamageInfo damageInfo)
+        {
+            if (__instance.creature.name.ToLower().Contains("leviathan"))
+                damageInfo.damage = 0f;
+        }
+    }
+}
