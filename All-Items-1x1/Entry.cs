7f7c30c36aa73c159623e8f942a10bef24a07f08
MrPurple6411 <mrpurple6411@gmail.com>
2019-12-26 15:24:58 -0700

    All Items 1x1

---
+++ All-Items-1x1/Entry.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace All_Items_1x1
+{
+    public class Entry
+    {
+        public static void Patch()
+        {
+            HarmonyInstance.Create("MrPurple6411.All_Items_1x1").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(CraftData))]
+    [HarmonyPatch(nameof(CraftData.GetItemSize))]
+    internal class Resizer
+    {
+        [HarmonyPostfix]
+        public static void postfix(TechType techType ,ref Vector2int __result)
+        {
+            __result = new Vector2int(1, 1);
+        }
+    }
+}
