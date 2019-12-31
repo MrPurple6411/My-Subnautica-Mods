5d5ffd3b737e9a5f166a8f9db386a969a30e0096
MrPurple6411 <mrpurple6411@gmail.com>
2019-12-30 21:36:51 -0700

    ABNM & IWL

---
+++ AmpeelBallsNoMore/Entry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Harmony;
+using WorldStreaming;
+
+namespace AmpeelBallsNoMore
+{
+    public static class Entry
+    {
+        public static void Patch()
+        {
+            HarmonyInstance.Create("MrPurple6411.AmpeelBallsNoMore").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(FleeOnDamage))]
+    [HarmonyPatch(nameof(FleeOnDamage.OnTakeDamage))]
+    internal class FleeOnDamage_StartPerform_Patch
+    {
+        [HarmonyPrefix]
+        static void Prefix(FleeOnDamage __instance, DamageInfo damageInfo)
+        {
+            if (__instance.creature.name.ToLower().Contains("shocker") && damageInfo.type == DamageType.Electrical)
+                damageInfo.damage = 0f;
+        }
+    }
+}
---
+++ IncreasedWorldLoad/Entry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Harmony;
+using WorldStreaming;
+
+namespace IncreasedWorldLoad
+{
+    public static class Entry
+    {
+        public static void Patch()
+        {
+            HarmonyInstance.Create("MrPurple6411.IncreasedWorldLoad").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldStreamer))]
+    [HarmonyPatch(nameof(WorldStreamer.ParseClipmapSettings))]
+    public class WorldStreamer_ParseClipmapSettings_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref ClipMapManager.Settings __result)
+        {
+            __result.maxThreads = Environment.ProcessorCount;
+            __result.maxWorkspaces *= Environment.ProcessorCount;
+            __result.maxMeshQueue *= Environment.ProcessorCount;
+
+            for (int i = 1; i < __result.levels.Length; i++)
+            {
+                ClipMapManager.LevelSettings levelSettings = __result.levels[i];
+                levelSettings.chunksPerSide = Environment.ProcessorCount;
+                levelSettings.chunksVertically = Environment.ProcessorCount;
+                levelSettings.grassSettings.reduction = 0;
+                levelSettings.skipRelax = true;
+                levelSettings.grass = true;
+                levelSettings.entities = true;
+            }
+        }
+    }
+}
