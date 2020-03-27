acb6204c69de97ae3bf3530ab32052d731701a12
MrPurple6411 <mrpurple6411@gmail.com>
2020-03-27 15:30:30 -0600

    BPOC

---
+++ BuilderPlaceOnComplete/Main.cs
@@ -0,0 +1,55 @@
+﻿using Harmony;
+using QModManager.API.ModLoading;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace BuilderPlaceOnComplete
+{
+	[QModCore]
+	public class Main
+	{
+
+		[QModPatch]
+		public void Load()
+		{
+			HarmonyInstance.Create("MrPurple6411.BuilderPlaceOnComplete").PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+
+	[HarmonyPatch(typeof(Builder), nameof(Builder.TryPlace))]
+	public class Builder_TryPlace
+	{
+		[HarmonyPrefix]
+		public static void Prefix()
+		{
+			Constructable_Construct.prefab = Builder.prefab;
+		}
+
+		[HarmonyPostfix]
+		public static void Postfix(bool __result)
+		{
+			if (!__result)
+			{
+				Constructable_Construct.prefab = null;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(Constructable), nameof(Constructable.Construct))]
+	public class Constructable_Construct
+	{
+		public static GameObject prefab;
+
+		[HarmonyPostfix]
+		public static void Postfix(Constructable __instance)
+		{
+			if(__instance.constructedAmount >= 1f)
+			{
+				Builder.Begin(prefab);
+			}
+		}
+	}
+}
