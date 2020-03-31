5bc14e87072973e00b5b48d7e729b84bbfb7b505
MrPurple6411 <mrpurple6411@gmail.com>
2020-03-31 07:36:57 -0600

    Copper From Scanning

---
+++ CopperFromScanning/Main.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using QModManager.API.ModLoading;
+using System.Reflection;
+
+namespace CopperFromScanning
+{
+    [HarmonyPatch(typeof(CraftData), nameof(CraftData.AddToInventory))]
+    internal class CraftData_AddToInventory
+    {
+        [HarmonyPrefix]
+        private static bool Prefix(TechType techType, int num = 1, bool noMessage = false, bool spawnIfCantAdd = true)
+        {
+            if(techType == TechType.Titanium && num == 2 && noMessage == false && spawnIfCantAdd == true)
+            {
+                CraftData.AddToInventory(TechType.Titanium);
+                CraftData.AddToInventory(TechType.Copper);
+                return false;
+            }
+            return true;
+        }
+    }
+}
