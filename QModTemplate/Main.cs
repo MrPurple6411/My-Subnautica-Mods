3a0806bfe509fea1f744da7783e3e597f18d773f
MrPurple6411 <mrpurple6411@gmail.com>
2020-04-01 12:54:54 -0600

    QmodTemplate

---
+++ QModTemplate/Main.cs
@@ -0,0 +1,17 @@
+﻿using Harmony;
+using QModManager.API.ModLoading;
+using System.Reflection;
+
+namespace QModTemplate
+{
+    [QModCore]
+    public class Main
+    {
+        [QModPatch]
+        public static void Load()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.Create($"YourName_{assembly.GetName().Name}").PatchAll(assembly);
+        }
+    }
+}
