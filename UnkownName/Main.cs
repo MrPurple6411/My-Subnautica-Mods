15fd399d4549913f2f81062e64398b5ae83c1ae4
MrPurple6411 <mrpurple6411@gmail.com>
2020-04-25 06:02:50 -0600

    Update 1.0.2
    Hide only missing ingredients from recipes instead of all.
    Items in inventory will no longer have names if they have not been unlocked with a scanner.
    Automatically scan things when picked up as long as you have a scanner in inventory.
    Fixed bug where things would not unlock properly sometimes when scanned.

--- UnkownName/Main.cs
+++ UnkownName/Main.cs
@@ -1,7 +1,10 @@
-﻿using Harmony;
+﻿using FMOD;
+using Harmony;
+using PriorityQueueInternal;
 using QModManager.API.ModLoading;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using UnityEngine;
@@ -14,38 +17,51 @@ namespace UnkownName
     {
 #if SUBNAUTICA
 
-        [HarmonyPrefix]
-        public static bool Prefix(ITechData data)
+        [HarmonyPostfix]
+        public static void Postfix(ITechData data, ref List<TooltipIcon> icons)
         {
             int ingredientCount = data.ingredientCount;
             for(int i = 0; i < ingredientCount; i++)
             {
                 IIngredient ingredient = data.GetIngredient(i);
                 TechType techType = ingredient.techType;
+                if(!KnownTech.Contains(techType) && PDAScanner.ContainsCompleteEntry(techType))
+                {
+                    KnownTech.Add(techType);
+                }
                 if(!KnownTech.Contains(techType) && GameModeUtils.RequiresBlueprints())
                 {
-                    return false;
+                    TooltipIcon icon = icons.Find((TooltipIcon) => TooltipIcon.sprite == SpriteManager.Get(techType) && TooltipIcon.text.Contains(Language.main.GetOrFallback(TooltipFactory.techTypeIngredientStrings.Get(techType), techType)));
+                    if(icons.Contains(icon))
+                    {
+                        icons.Remove(icon);
+                    }
                 }
             }
-
-            return true;
         }
 
 #elif BELOWZERO
 
-        [HarmonyPrefix]
-        public static bool Prefix(IList<Ingredient> ingredients)
+        [HarmonyPostfix]
+        public static void Postfix(IList<Ingredient> ingredients, ref List<TooltipIcon> icons)
         {
             int ingredientCount = ingredients.Count;
             for(int i = 0; i < ingredientCount; i++)
             {
-                if(!KnownTech.Contains(ingredients[i].techType) && GameModeUtils.RequiresBlueprints())
+                TechType techType = ingredients[i].techType;
+                if(!KnownTech.Contains(techType) && PDAScanner.ContainsCompleteEntry(techType))
                 {
-                    return false;
+                    KnownTech.Add(techType, true);
+                }
+                if(!KnownTech.Contains(techType) && GameModeUtils.RequiresBlueprints())
+                {
+                    TooltipIcon icon = icons.Find((TooltipIcon) => TooltipIcon.sprite == SpriteManager.Get(techType) && TooltipIcon.text.Contains(Language.main.GetOrFallback(TooltipFactory.techTypeIngredientStrings.Get(techType), techType)));
+                    if(icons.Contains(icon))
+                    {
+                        icons.Remove(icon);
+                    }
                 }
             }
-
-            return true;
         }
 
 #endif
@@ -64,6 +80,23 @@ namespace UnkownName
                 TooltipFactory.WriteDescription(stringBuilder, "???????");
                 tooltipText = stringBuilder.ToString();
             }
+        }
+    }
+
+
+    [HarmonyPatch(typeof(TooltipFactory), nameof(TooltipFactory.ItemCommons))]
+    public class TooltipFactory_InventoryItem
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref StringBuilder sb, TechType techType, GameObject obj)
+        {
+            if(!PDAScanner.CanScan(obj) || PDAScanner.ContainsCompleteEntry(techType) || KnownTech.Contains(techType))
+            {
+                return;
+            }
+            sb.Clear();
+            TooltipFactory.WriteTitle(sb, "???????");
+            TooltipFactory.WriteDescription(sb, "???????");
         }
     }
 
@@ -89,15 +122,14 @@ namespace UnkownName
         public static void Postfix(ScannerTool __instance)
         {
             PDAScanner.ScanTarget scanTarget = PDAScanner.scanTarget;
-            if(__instance.energyMixin.charge <= 0f || !scanTarget.isValid || PDAScanner.CanScan() != PDAScanner.Result.Scan || __instance.stateCurrent == ScannerTool.ScanState.SelfScan || !GameModeUtils.RequiresBlueprints())
+            PDAScanner.Result result = PDAScanner.CanScan();
+
+            if(PDAScanner.ContainsCompleteEntry(scanTarget.techType) || __instance.energyMixin.charge <= 0f || !scanTarget.isValid || result != PDAScanner.Result.Scan || __instance.stateCurrent == ScannerTool.ScanState.SelfScan || !GameModeUtils.RequiresBlueprints())
             {
                 return;
             }
-
             HandReticle main = HandReticle.main;
             main.SetInteractText("???????", true, HandReticle.Hand.Right);
-            main.SetIcon(HandReticle.IconType.Scan, 1.5f);
-            __instance.UpdateScreen(ScannerTool.ScreenState.Ready, 0f);
         }
 
 #elif BELOWZERO
@@ -106,15 +138,13 @@ namespace UnkownName
         public static void Postfix(ScannerTool __instance)
         {
             PDAScanner.ScanTarget scanTarget = PDAScanner.scanTarget;
-            if(__instance.energyMixin.charge <= 0f || !scanTarget.isValid || PDAScanner.CanScan() != PDAScanner.Result.Scan || !GameModeUtils.RequiresBlueprints())
+            PDAScanner.Result result = PDAScanner.CanScan();
+            if(PDAScanner.ContainsCompleteEntry(scanTarget.techType) || __instance.energyMixin.charge <= 0f || !scanTarget.isValid || result != PDAScanner.Result.Scan || !GameModeUtils.RequiresBlueprints())
             {
                 return;
             }
-
             HandReticle main = HandReticle.main;
             main.SetText(HandReticle.TextType.Hand, "???????", true, GameInput.Button.RightHand);
-            main.SetIcon(HandReticle.IconType.Scan, 1.5f);
-            __instance.UpdateScreen(ScannerTool.ScreenState.Ready, 0f);
         }
 
 #endif
@@ -126,15 +156,61 @@ namespace UnkownName
         [HarmonyPostfix]
         public static void Postfix(Pickupable pickupable)
         {
-            if(!KnownTech.Contains(pickupable.GetTechType()) && GameModeUtils.RequiresBlueprints())
+            TechType techType = pickupable.GetTechType();
+            PDAScanner.EntryData entryData = PDAScanner.GetEntryData(techType);
+            GameObject gameObject = pickupable.gameObject;
+            if(Inventory.main.container.Contains(TechType.Scanner) || techType == TechType.AcidMushroom || techType == TechType.Copper)
             {
-                KnownTech.Add(pickupable.GetTechType());
+                bool flag6 = false;
+                bool flag7 = false;
+                if(entryData != null)
+                {
+                    flag7 = true;
+                    if(!PDAScanner.GetPartialEntryByKey(techType, out PDAScanner.Entry entry))
+                    {
+                        entry = PDAScanner.Add(techType, 0);
+                    }
+                    if(entry != null)
+                    {
+                        flag6 = true;
+                        PDAScanner.partial.Remove(entry);
+                        PDAScanner.complete.Add(entry.techType);
+                        PDAScanner.NotifyRemove(entry);
+                    }
+                }
+                if(gameObject != null)
+                {
+                    gameObject.SendMessage("OnScanned", null, SendMessageOptions.DontRequireReceiver);
+                }
+                ResourceTracker.UpdateFragments();
+                if(flag6 || flag7)
+                {
+                    PDAScanner.Unlock(entryData, flag6, flag7, true);
+                }
+                KnownTech.Add(techType, true);
             }
         }
     }
 
+    [HarmonyPatch(typeof(KnownTech), nameof(KnownTech.Analyze))]
+    public class KnownTech_Analyze
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(TechType techType)
+        {
+            PDAScanner.EntryData entryData = PDAScanner.GetEntryData(techType);
+            return techType == TechType.None
+                ? true
+                : entryData == null
+                ? true
+                : !PDAScanner.ContainsCompleteEntry(techType)
+                ? true
+                : string.IsNullOrEmpty(entryData.encyclopedia) || PDAEncyclopedia.ContainsEntry(entryData.encyclopedia);
+        }
+    }
+
     [HarmonyPatch(typeof(GUIHand), nameof(GUIHand.OnUpdate))]
-    public class GUIHand_Send
+    public class GUIHand_OnUpdate
     {
 #if SUBNAUTICA
 
@@ -166,12 +242,22 @@ namespace UnkownName
     [HarmonyPatch(typeof(PDAScanner), nameof(PDAScanner.Scan), new Type[] { })]
     public class PDAScanner_Scan
     {
+        public static TechType techType;
+        [HarmonyPrefix]
+        public static void Prefix()
+        {
+            if(PDAScanner.scanTarget.techType != TechType.None)
+            {
+                techType = PDAScanner.scanTarget.techType;
+            }
+        }
+
         [HarmonyPostfix]
         public static void Postfix(ref PDAScanner.Result __result)
         {
-            if((__result == PDAScanner.Result.Done || __result == PDAScanner.Result.Researched) && !KnownTech.Contains(PDAScanner.scanTarget.techType))
+            if(__result != PDAScanner.Result.Scan && !KnownTech.Contains(techType))
             {
-                KnownTech.Add(PDAScanner.scanTarget.techType);
+                KnownTech.Add(techType);
             }
         }
     }
