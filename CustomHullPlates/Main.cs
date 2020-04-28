948f2e0ac502a8d21f01663bc691f845184ba7a0
MrPurple6411 <mrpurple6411@gmail.com>
2020-04-28 05:57:41 -0600

    Custom Hull Plates And Custom Posters

---
+++ CustomHullPlates/Main.cs
@@ -0,0 +1,63 @@
+﻿using Oculus.Newtonsoft.Json;
+using QModManager.API.ModLoading;
+using SMLHelper.V2.Utility;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace CustomHullPlates
+{
+    [QModCore]
+    public class Main
+    {
+        private static readonly DirectoryInfo HullPlateFolder = Directory.CreateDirectory(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "HullPlates"));
+        private static readonly string ModName = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [QModPatch]
+        public static void Load()
+        {
+            foreach(string directory in Directory.GetDirectories(HullPlateFolder.FullName))
+            {
+                string info = Path.Combine(directory, "info.json");
+                string icon = Path.Combine(directory, "icon.png");
+                string texture = Path.Combine(directory, "texture.png");
+                if(File.Exists(info) && File.Exists(icon) && File.Exists(texture))
+                {
+                    try
+                    {
+                        HullPlateInfo hullPlate;
+                        using(StreamReader reader = new StreamReader(info))
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            hullPlate = serializer.Deserialize(reader, typeof(HullPlateInfo)) as HullPlateInfo;
+                        }
+
+                        Texture2D icontexture = ImageUtils.LoadTextureFromFile(icon);
+                        Texture2D hullPlateTexture = ImageUtils.LoadTextureFromFile(texture);
+
+                        if(hullPlate != null && icontexture != null && hullPlateTexture != null)
+                        {
+                            new BasicHullPlatePrefab(hullPlate.InternalName, hullPlate.DisplayName, hullPlate.Description, icontexture, hullPlateTexture).Patch();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"[{ModName}] Unable to load Custom Hull Plate from {Path.GetDirectoryName(directory)}!");
+                        }
+                        
+                    }
+                    catch(Exception)
+                    {
+                        Console.WriteLine($"[{ModName}] Unable to load Custom Hull Plate from {Path.GetDirectoryName(directory)}!");
+                    }
+                }
+            }
+        }
+    }
+    public class HullPlateInfo
+    {
+        public string InternalName { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ CustomPosters/BasicPostersPrefab.cs
@@ -0,0 +1,102 @@
+﻿using FMOD;
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using UnityEngine;
+
+namespace CustomHullPlates
+{
+    public class BasicPostersPrefab: Buildable
+    {
+        readonly Texture2D posterIcon;
+        readonly Texture2D posterTexture;
+        readonly string orientation;
+
+        public BasicPostersPrefab(string classId, string friendlyName, string description, string orientation, Texture2D posterIcon, Texture2D posterTexture) : base(classId, friendlyName, description)
+        {
+            this.orientation = orientation;
+            this.posterIcon = posterIcon;
+            this.posterTexture = posterTexture;
+            this.TechType = TechTypeHandler.AddTechType(classId, friendlyName, description);
+        }
+
+        public override TechGroup GroupForPDA => TechGroup.Miscellaneous;
+
+        public override TechCategory CategoryForPDA => TechCategory.MiscHullplates;
+
+        public override GameObject GetGameObject()
+        {
+            GameObject _GameObject;
+            if(this.orientation.ToLower() == "landscape")
+            {
+
+                _GameObject = CraftData.InstantiateFromPrefab(TechType.PosterAurora);
+            }
+            else
+            {
+                _GameObject = CraftData.InstantiateFromPrefab(TechType.PosterKitty);
+            }
+
+            _GameObject.name = ClassID;
+
+            Constructable constructable = _GameObject.EnsureComponent<Constructable>();
+            constructable.allowedInBase = true;
+            constructable.allowedInSub = true;
+            constructable.allowedOnConstructables = true;
+            constructable.allowedOnWall = true;
+            constructable.allowedOutside = true;
+            constructable.constructedAmount = 1;
+
+            MeshRenderer meshRenderer = _GameObject.GetComponentInChildren<MeshRenderer>();
+            foreach (Material material in meshRenderer.materials.Where((m)=> !m.name.Contains("magnet")) ?? new List<Material>())
+            {
+                Texture2D blankTexture = new Texture2D(posterTexture.width, posterTexture.height, TextureFormat.ARGB32, false);
+
+                material.SetTexture("_MainTex", posterTexture);
+                material.SetTexture("_SpecTex", blankTexture);
+            }
+
+            constructable.model = _GameObject;
+            Pickupable pickupable = _GameObject.GetComponentInChildren<Pickupable>();
+            pickupable.isPickupable = false;
+            pickupable.overrideTechType = this.TechType;
+
+            Main.customPosters.Add(ClassID);
+            return _GameObject;
+        }
+
+#if SUBNAUTICA
+        /// <summary>
+        /// This provides the <see cref="TechData"/> instance used to designate how this item is crafted or constructed.
+        /// </summary>
+        protected override TechData GetBlueprintRecipe()
+        {
+            return new TechData(new Ingredient(TechType.Titanium, 1));
+        }
+
+        protected override Atlas.Sprite GetItemSprite()
+        {
+            return ImageUtils.LoadSpriteFromTexture(posterIcon);
+        }
+#elif BELOWZERO
+        /// <summary>
+        /// This provides the <see cref="RecipeData"/> instance used to designate how this item is crafted or constructed.
+        /// </summary>
+        protected override RecipeData GetBlueprintRecipe()
+        {
+            return new RecipeData(new Ingredient(TechType.Titanium, 1), new Ingredient(TechType.Glass, 1));
+        }
+        
+        protected override Sprite GetItemSprite()
+        {
+            return ImageUtils.LoadSpriteFromTexture(posterIcon);
+        }
+#endif
+
+    }
+}
