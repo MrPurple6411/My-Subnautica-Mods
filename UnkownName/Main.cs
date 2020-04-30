1f2e22e5f54fd80c96d7ce2fa8674c95d1aaff1b
MrPurple6411 <mrpurple6411@gmail.com>
2020-04-30 02:21:19 -0600

    1.0.3 and 1.0.4

--- UnkownName/Main.cs
+++ UnkownName/Main.cs
@@ -1,17 +1,176 @@
-﻿using FMOD;
-using Harmony;
-using PriorityQueueInternal;
-using QModManager.API.ModLoading;
+﻿using Harmony;
+using Oculus.Newtonsoft.Json;
+using Oculus.Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using UnityEngine;
-using UnityEngine.UI;
+using UWE;
 
 namespace UnkownName
 {
+    [HarmonyPatch(typeof(MainMenuRightSide), nameof(MainMenuRightSide.Awake))]
+    public class UnknownNameConfig
+    {
+        public static readonly string config = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "config.json");
+        public static string UnkownLabel = "";
+        public static string UnkownTitle = "Unanalyzed Item";
+        public static string UnkownDescription = "This item has not been analyzed yet. \nTo discover its usefulness please scan it at your earliest convenience.";
+        public static bool ScanOnPickup = false;
+        public static bool Hardcore = false;
+
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            if(File.Exists(config))
+            {
+                using(StreamReader reader = new StreamReader(config))
+                {
+                    JObject json = JsonConvert.DeserializeObject<JObject>(reader.ReadToEnd());
+                    if(json["UnkownLabel"] != null)
+                    {
+                        UnkownLabel = json["UnkownLabel"].ToString();
+                    }
+                    if(json["UnkownTitle"] != null)
+                    {
+                        UnkownTitle = json["UnkownTitle"].ToString();
+                    }
+                    if(json["UnkownDescription"] != null)
+                    {
+                        UnkownDescription = json["UnkownDescription"].ToString();
+                    }
+                    if(json["ScanOnPickup"] != null)
+                    {
+                        ScanOnPickup = json["ScanOnPickup"].ToString().ToLower() == "true";
+                    }
+                    if(json["Hardcore"] != null)
+                    {
+                        Hardcore = json["Hardcore"].ToString().ToLower() == "true";
+                    }
+                }
+            }
+            else
+            {
+                using(StreamWriter writer = new StreamWriter(config))
+                {
+                    JObject json = new JObject
+                    {
+                        ["UnkownLabel"] = UnkownLabel,
+                        ["UnkownTitle"] = UnkownTitle,
+                        ["UnkownDescription"] = UnkownDescription,
+                        ["ScanOnPickup"] = ScanOnPickup,
+                        ["Hardcore"] = Hardcore
+                    };
+                    writer.Write(json.ToString());
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(CrafterLogic), nameof(CrafterLogic.IsCraftRecipeUnlocked))]
+    public class CraftNode_SetVisable
+    {
+        [HarmonyPostfix]
+        public static void Postfix(TechType techType, ref bool __result)
+        {
+            if(UnknownNameConfig.Hardcore && GameModeUtils.RequiresBlueprints() && __result)
+            {
+#if SUBNAUTICA
+                ITechData data = CraftData.Get(techType, true);
+                if(data != null)
+                {
+                    int ingredientCount = data.ingredientCount;
+                    for(int i = 0; i < ingredientCount; i++)
+                    {
+                        IIngredient ingredient = data.GetIngredient(i);
+                        if(!CrafterLogic.IsCraftRecipeUnlocked(ingredient.techType))
+                        {
+                            __result = false;
+                            return;
+                        }
+                    }
+                }
+#elif BELOWZERO
+                List<Ingredient> data = TechData.GetIngredients(techType)?.ToList() ?? new List<Ingredient>();
+                int ingredientCount = data.Count;
+                for(int i = 0; i < ingredientCount; i++)
+                {
+                    Ingredient ingredient = data[i];
+                    if(!CrafterLogic.IsCraftRecipeUnlocked(ingredient.techType))
+                    {
+                        __result = false;
+                        return;
+                    }
+                }
+#endif
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(IntroVignette), nameof(IntroVignette.ShouldPlayIntro))]
+    public class IntroVignette_ShouldPlayIntro
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref bool __result)
+        {
+            if(UnknownNameConfig.Hardcore)
+            {
+                __result = false;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Player), nameof(Player.Start))]
+    public class Player_FirstStart
+    {
+        [HarmonyPrefix]
+        public static void Prefix()
+        {
+            if(File.Exists(UnknownNameConfig.config))
+            {
+                using(StreamReader reader = new StreamReader(UnknownNameConfig.config))
+                {
+                    JObject json = JsonConvert.DeserializeObject<JObject>(reader.ReadToEnd());
+                    if(json["UnkownLabel"] != null)
+                    {
+                        UnknownNameConfig.UnkownLabel = json["UnkownLabel"].ToString();
+                    }
+                    if(json["UnkownTitle"] != null)
+                    {
+                        UnknownNameConfig.UnkownTitle = json["UnkownTitle"].ToString();
+                    }
+                    if(json["UnkownDescription"] != null)
+                    {
+                        UnknownNameConfig.UnkownDescription = json["UnkownDescription"].ToString();
+                    }
+                    if(json["ScanOnPickup"] != null)
+                    {
+                        UnknownNameConfig.ScanOnPickup = json["ScanOnPickup"].ToString().ToLower() == "true";
+                    }
+                    if(json["Hardcore"] != null)
+                    {
+                        UnknownNameConfig.Hardcore = json["Hardcore"].ToString().ToLower() == "true";
+                    }
+                }
+            }
+            if(UnknownNameConfig.Hardcore)
+            {
+                GameModeUtils.ActivateCheat(GameModeOption.NoHints);
+            }
+            if(UnknownNameConfig.Hardcore && !Utils.GetContinueMode())
+            {
+                Pickupable pickupable = CraftData.InstantiateFromPrefab(TechType.Scanner).GetComponent<Pickupable>();
+                ScannerTool scannerTool = pickupable.GetComponent<ScannerTool>();
+                Pickupable pickupable1 = CraftData.InstantiateFromPrefab(TechType.Battery).GetComponent<Pickupable>();
+                scannerTool.energyMixin.batterySlot.AddItem(pickupable1);
+                Inventory.main.container.AddItem(pickupable);
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(TooltipFactory), nameof(TooltipFactory.WriteIngredients))]
     public class TooltipFactory_WriteIngredients
     {
@@ -35,6 +194,8 @@ namespace UnkownName
                     if(icons.Contains(icon))
                     {
                         icons.Remove(icon);
+                        TooltipIcon tooltipIcon = new TooltipIcon() { sprite = SpriteManager.Get(TechType.None), text = UnknownNameConfig.UnkownTitle };
+                        icons.Add(tooltipIcon);
                     }
                 }
             }
@@ -59,6 +220,8 @@ namespace UnkownName
                     if(icons.Contains(icon))
                     {
                         icons.Remove(icon);
+                        TooltipIcon tooltipIcon = new TooltipIcon() { sprite = SpriteManager.Get(TechType.None), text = UnknownNameConfig.UnkownTitle };
+                        icons.Add(tooltipIcon);
                     }
                 }
             }
@@ -76,13 +239,12 @@ namespace UnkownName
             StringBuilder stringBuilder = new StringBuilder();
             if(locked && GameModeUtils.RequiresBlueprints())
             {
-                TooltipFactory.WriteTitle(stringBuilder, "???????");
-                TooltipFactory.WriteDescription(stringBuilder, "???????");
+                TooltipFactory.WriteTitle(stringBuilder, UnknownNameConfig.UnkownTitle);
+                TooltipFactory.WriteDescription(stringBuilder, UnknownNameConfig.UnkownDescription);
                 tooltipText = stringBuilder.ToString();
             }
         }
     }
-
 
     [HarmonyPatch(typeof(TooltipFactory), nameof(TooltipFactory.ItemCommons))]
     public class TooltipFactory_InventoryItem
@@ -95,8 +257,8 @@ namespace UnkownName
                 return;
             }
             sb.Clear();
-            TooltipFactory.WriteTitle(sb, "???????");
-            TooltipFactory.WriteDescription(sb, "???????");
+            TooltipFactory.WriteTitle(sb, UnknownNameConfig.UnkownTitle);
+            TooltipFactory.WriteDescription(sb, UnknownNameConfig.UnkownDescription);
         }
     }
 
@@ -108,7 +270,7 @@ namespace UnkownName
         {
             if(!KnownTech.Contains(techType) && GameModeUtils.RequiresBlueprints())
             {
-                __result = "???????";
+                __result = UnknownNameConfig.UnkownLabel;
             }
         }
     }
@@ -116,38 +278,24 @@ namespace UnkownName
     [HarmonyPatch(typeof(ScannerTool), nameof(ScannerTool.OnHover))]
     public class ScannerTool_OnHover
     {
+        [HarmonyPostfix]
+        public static void Postfix(ScannerTool __instance)
+        {
+            PDAScanner.ScanTarget scanTarget = PDAScanner.scanTarget;
+            PDAScanner.Result result = PDAScanner.CanScan();
+            PDAScanner.EntryData entryData = PDAScanner.GetEntryData(PDAScanner.scanTarget.techType);
+
+            if((entryData != null && KnownTech.Contains(entryData.blueprint)) || PDAScanner.ContainsCompleteEntry(scanTarget.techType) || __instance.energyMixin.charge <= 0f || !scanTarget.isValid || result != PDAScanner.Result.Scan || !GameModeUtils.RequiresBlueprints())
+            {
+                return;
+            }
 #if SUBNAUTICA
-
-        [HarmonyPostfix]
-        public static void Postfix(ScannerTool __instance)
-        {
-            PDAScanner.ScanTarget scanTarget = PDAScanner.scanTarget;
-            PDAScanner.Result result = PDAScanner.CanScan();
-
-            if(PDAScanner.ContainsCompleteEntry(scanTarget.techType) || __instance.energyMixin.charge <= 0f || !scanTarget.isValid || result != PDAScanner.Result.Scan || __instance.stateCurrent == ScannerTool.ScanState.SelfScan || !GameModeUtils.RequiresBlueprints())
-            {
-                return;
-            }
-            HandReticle main = HandReticle.main;
-            main.SetInteractText("???????", true, HandReticle.Hand.Right);
-        }
-
+            HandReticle.main.SetInteractText(UnknownNameConfig.UnkownLabel, false, HandReticle.Hand.None);
 #elif BELOWZERO
-
-        [HarmonyPostfix]
-        public static void Postfix(ScannerTool __instance)
-        {
-            PDAScanner.ScanTarget scanTarget = PDAScanner.scanTarget;
-            PDAScanner.Result result = PDAScanner.CanScan();
-            if(PDAScanner.ContainsCompleteEntry(scanTarget.techType) || __instance.energyMixin.charge <= 0f || !scanTarget.isValid || result != PDAScanner.Result.Scan || !GameModeUtils.RequiresBlueprints())
-            {
-                return;
-            }
-            HandReticle main = HandReticle.main;
-            main.SetText(HandReticle.TextType.Hand, "???????", true, GameInput.Button.RightHand);
+            HandReticle.main.SetText(HandReticle.TextType.Hand, UnknownNameConfig.UnkownLabel, true, GameInput.Button.None);
+#endif
         }
 
-#endif
     }
 
     [HarmonyPatch(typeof(Inventory), nameof(Inventory.Pickup))]
@@ -159,7 +307,7 @@ namespace UnkownName
             TechType techType = pickupable.GetTechType();
             PDAScanner.EntryData entryData = PDAScanner.GetEntryData(techType);
             GameObject gameObject = pickupable.gameObject;
-            if(Inventory.main.container.Contains(TechType.Scanner) || techType == TechType.AcidMushroom || techType == TechType.Copper)
+            if(UnknownNameConfig.ScanOnPickup && Inventory.main.container.Contains(TechType.Scanner))
             {
                 bool flag6 = false;
                 bool flag7 = false;
@@ -186,8 +334,8 @@ namespace UnkownName
                 if(flag6 || flag7)
                 {
                     PDAScanner.Unlock(entryData, flag6, flag7, true);
+                    KnownTech.Add(techType, true);
                 }
-                KnownTech.Add(techType, true);
             }
         }
     }
@@ -203,7 +351,7 @@ namespace UnkownName
                 ? true
                 : entryData == null
                 ? true
-                : !PDAScanner.ContainsCompleteEntry(techType)
+                : PDAScanner.ContainsCompleteEntry(techType)
                 ? true
                 : string.IsNullOrEmpty(entryData.encyclopedia) || PDAEncyclopedia.ContainsEntry(entryData.encyclopedia);
         }
@@ -212,31 +360,22 @@ namespace UnkownName
     [HarmonyPatch(typeof(GUIHand), nameof(GUIHand.OnUpdate))]
     public class GUIHand_OnUpdate
     {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            PDAScanner.ScanTarget scanTarget = PDAScanner.scanTarget;
+            PDAScanner.EntryData entryData = PDAScanner.GetEntryData(scanTarget.techType);
+
+            if((entryData != null && KnownTech.Contains(entryData.blueprint)) || !scanTarget.isValid || PDAScanner.CanScan() != PDAScanner.Result.Scan || !GameModeUtils.RequiresBlueprints())
+            {
+                return;
+            }
 #if SUBNAUTICA
-
-        [HarmonyPostfix]
-        public static void Postfix()
-        {
-            PDAScanner.ScanTarget scanTarget = PDAScanner.scanTarget;
-            if(scanTarget.isValid && PDAScanner.CanScan() == PDAScanner.Result.Scan && !PDAScanner.scanTarget.isPlayer && GameModeUtils.RequiresBlueprints())
-            {
-                HandReticle.main.SetInteractText("???????", false, HandReticle.Hand.None);
-            }
-        }
-
+            HandReticle.main.SetInteractText(UnknownNameConfig.UnkownLabel, false, HandReticle.Hand.None);
 #elif BELOWZERO
-
-        [HarmonyPostfix]
-        public static void Postfix()
-        {
-            PDAScanner.ScanTarget scanTarget = PDAScanner.scanTarget;
-            if(scanTarget.isValid && PDAScanner.CanScan() == PDAScanner.Result.Scan && GameModeUtils.RequiresBlueprints())
-            {
-                HandReticle.main.SetText(HandReticle.TextType.Hand, "???????", true, GameInput.Button.None);
-            }
-        }
-
+            HandReticle.main.SetText(HandReticle.TextType.Hand, UnknownNameConfig.UnkownLabel, true, GameInput.Button.None);
 #endif
+        }
     }
 
     [HarmonyPatch(typeof(PDAScanner), nameof(PDAScanner.Scan), new Type[] { })]
@@ -258,6 +397,15 @@ namespace UnkownName
             if(__result != PDAScanner.Result.Scan && !KnownTech.Contains(techType))
             {
                 KnownTech.Add(techType);
+#if SUBNAUTICA
+                TechType techType2 = CraftData.GetHarvestOutputData(techType);
+#elif BELOWZERO
+                TechType techType2 = TechData.GetHarvestOutput(techType);
+#endif
+                if(techType2 != TechType.None)
+                {
+                    KnownTech.Add(techType2);
+                }
             }
         }
     }
