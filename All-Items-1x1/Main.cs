7c1a9be057a9446491d96a4d296438e9cee84933
MrPurple6411 <mrpurple6411@gmail.com>
2020-05-19 02:02:06 -0600

    BZ FIX

--- All-Items-1x1/Main.cs
+++ All-Items-1x1/Main.cs
@@ -1,16 +1,22 @@
-﻿using Harmony;
+﻿using System.Reflection;
+using Harmony;
+using QModManager.API.ModLoading;
 
 namespace All_Items_1x1
 {
+    [QModCore]
+    public class Main
+    {
+        [QModPatch]
+        public static void Load()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.Create($"MrPurple6411_{assembly.GetName().Name}").PatchAll(assembly);
+        }
+    }
 #if SUBNAUTICA
 
-    [HarmonyPatch(typeof(CraftData))]
-    [HarmonyPatch(nameof(CraftData.GetItemSize))]
-#elif BELOWZERO
-
-    [HarmonyPatch(typeof(TechData))]
-    [HarmonyPatch(nameof(TechData.GetItemSize))]
-#endif
+    [HarmonyPatch(typeof(CraftData), nameof(CraftData.GetItemSize))]
     public class Resizer
     {
         [HarmonyPostfix]
@@ -19,4 +25,15 @@ namespace All_Items_1x1
             __result = new Vector2int(1, 1);
         }
     }
+#elif BELOWZERO
+    [HarmonyPatch(typeof(TechData), nameof(TechData.GetItemSize))]
+    public class Resizer
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref Vector2int __result)
+        {
+            __result = new Vector2int(1, 1);
+        }
+    }
+#endif
 }
