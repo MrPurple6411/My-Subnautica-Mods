2b327d6827b9dc0057e02a23294979207fd0fea1
MrPurple6411 <mrpurple6411@gmail.com>
2020-05-19 02:03:26 -0600

    More_Dillable_Resources

---
+++ More Dillable Resources/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QModManager.API.ModLoading;
+using UWE;
+using static LootDistributionData;
+
+namespace More_Drillable_Resources
+{
+    [QModCore]
+    public class Main
+    {
+        [QModPatch]
+        public static void Load()
+        {
+            LootDistributionData data = LootDistributionData.Load("Balance/EntityDistributions");
+            List<TechType> Drillables = Enum.GetValues(typeof(TechType)).OfType<TechType>().Where((tt) => tt.AsString().Contains("Drillable")).ToList();
+            data.GetPrefabData(CraftData.GetClassIdForTechType(TechType.DrillableUranium), out SrcData UraniumData);
+            foreach (TechType techType in Drillables)
+            {
+                string classId = CraftData.GetClassIdForTechType(techType);
+
+                if (CraftData.GetPrefabForTechType(techType, false) == null)
+                {
+                    continue;
+                }
+
+                if (WorldEntityDatabase.TryGetInfo(classId, out WorldEntityInfo info))
+                {
+                    if (!data.GetPrefabData(classId, out SrcData srcData))
+                    {
+                        SMLHelper.V2.Handler.LootDistributionHandler.AddLootDistributionData(classId, UraniumData);
+                    }
+                }
+            }
+        }
+    }
+}
