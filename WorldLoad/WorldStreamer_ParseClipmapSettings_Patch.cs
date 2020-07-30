17cd3276811c2d421a6c8089f850155db456965f
MrPurple6411 <mrpurple6411@gmail.com>
2020-07-30 15:09:33 -0600

    QMM 4.0 Version Bump

--- WorldLoad/WorldStreamer_ParseClipmapSettings_Patch.cs
+++ WorldLoad/WorldStreamer_ParseClipmapSettings_Patch.cs
@@ -14,12 +14,11 @@ namespace WorldLoad
             __result.maxWorkspaces *= 4;
             __result.maxMeshQueue *= 4;
 
-            for (int i = 1; i < __result.levels.Length; i++)
+            for (int i = 1; i < __result.levels.Length-1; i++)
             {
                 ClipMapManager.LevelSettings levelSettings = __result.levels[i];
                 levelSettings.chunksPerSide = Main.config.IncreasedWorldLoad;
                 levelSettings.chunksVertically = Main.config.IncreasedWorldLoad;
-                levelSettings.entities = true;
                 levelSettings.grass = true;
                 levelSettings.grassSettings.reduction = 0f;
             }
