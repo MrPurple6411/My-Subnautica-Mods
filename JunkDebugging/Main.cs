cfe64b2249e5b91ece84e7f27e99af4aa5237760
MrPurple6411 <mrpurple6411@gmail.com>
2020-08-11 09:43:17 -0600

    Basic Junk tool

---
+++ JunkDebugging/Main.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Oculus.Newtonsoft.Json;
+using QModManager.API.ModLoading;
+using SMLHelper.V2.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using static LootDistributionData;
+
+namespace JunkDebugging
+{
+    [QModCore]
+    public static class Main
+    {
+        [QModPatch]
+        public static void Load()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            new Harmony($"MrPurple6411_{assembly.GetName().Name}").PatchAll(assembly);
+        }
+
+    }
+
+}
