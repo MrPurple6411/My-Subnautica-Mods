4e21d85934daf01496beb18c303d3ff9bdc3a76e
MrPurple6411 <mrpurple6411@gmail.com>
2020-08-13 17:41:35 -0600

    Quantum Beginnings

---
+++ QuantumPowerTransmitters/Main.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using Oculus.Newtonsoft.Json;
+using QModManager.API.ModLoading;
+using QuantumPowerTransmitters.Prefabs;
+using SMLHelper.V2.Handlers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace QuantumPowerTransmitters
+{
+    [QModCore]
+    public static class Main
+    {
+        internal static TransmitterDishPrefab transmitterDish = new TransmitterDishPrefab();
+        internal static List<BasePowerRelay> baseRelays = new List<BasePowerRelay>();
+
+        [QModPatch]
+        public static void Load()
+        {
+            //Assembly assembly = Assembly.GetExecutingAssembly();
+            //new Harmony($"MrPurple6411_{assembly.GetName().Name}").PatchAll(assembly);
+
+            transmitterDish.Patch();
+        }
+    }
+}
---
+++ QuantumPowerTransmitters/MonoBehaviours/PowerConsolidation.cs
@@ -0,0 +1,100 @@
+﻿using HarmonyLib;
+using Oculus.Newtonsoft.Json;
+using QModManager.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Logger = QModManager.Utility.Logger;
+
+namespace QuantumPowerTransmitters.MonoBehaviours
+{
+    internal class PowerConsolidation : MonoBehaviour, IPowerInterface
+    {
+        private List<BasePowerRelay> baseRelays => Main.baseRelays;
+        private BasePowerRelay baseRelay;
+        private Constructable constructable;
+        private BasePowerRelay[] otherRelays => baseRelays.Where((x)=> x.gameObject.GetInstanceID() != baseRelay.gameObject.GetInstanceID()).ToArray();
+        private List<IPowerInterface> PowerInterfaces = new List<IPowerInterface>();
+        private float nextcheck = 0;
+
+        private void Start()
+        {
+            baseRelay = this.gameObject.GetComponentInParent<BasePowerRelay>();
+            constructable = this.gameObject.GetComponent<Constructable>();
+            if(baseRelay != null && !baseRelays.Contains(baseRelay))
+            {
+                baseRelays.Add(baseRelay);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if(constructable == null)
+            {
+                constructable = this.gameObject.GetComponent<Constructable>();
+            }
+
+            if (constructable.constructed)
+            {
+                if(baseRelay == null)
+                {
+                    baseRelay = this.gameObject.GetComponentInParent<BasePowerRelay>();
+                }
+
+                if (!baseRelays.Contains(baseRelay))
+                {
+                    baseRelays.Add(baseRelay);
+                }
+
+                float time = Time.time;
+                if (time > nextcheck)
+                {
+                    Logger.Log(Logger.Level.Info, $"Relays: " + JsonConvert.SerializeObject(baseRelays, Formatting.Indented, new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }), showOnScreen: true);
+                    /*foreach (BasePowerRelay basePower in otherRelays)
+                    {
+                        List<IPowerInterface> interfaces = (List<IPowerInterface>)AccessTools.Field(typeof(BasePowerRelay), "inboundPowerSources").GetValue(basePower);
+                        PowerInterfaces.Concat(interfaces);
+                    }*/
+                    nextcheck += 15;
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (baseRelays.Contains(baseRelay))
+            {
+                baseRelays.Remove(baseRelay);
+            }
+        }
+
+        public bool GetInboundHasSource(IPowerInterface powerInterface)
+        {
+            return PowerInterfaces.Contains(powerInterface);
+        }
+
+        public float GetMaxPower()
+        {
+            return PowerInterfaces.Sum((x) => x.GetMaxPower());
+        }
+
+        public float GetPower()
+        {
+            return PowerInterfaces.Sum((x) => x.GetPower());
+        }
+
+        public bool HasInboundPower(IPowerInterface powerInterface)
+        {
+            return PowerInterfaces.Contains(powerInterface);
+        }
+
+        public bool ModifyPower(float amount, out float modified)
+        {
+            modified = 0;
+            return false;
+        }
+    }
+}
