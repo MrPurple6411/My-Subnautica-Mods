0ae5a35023c846197a8b5c5ec28fe60f2a239a64
MrPurple6411 <mrpurple6411@gmail.com>
2020-10-13 22:19:49 -0600

    Renaming some assets

--- UnobtaniumBatteries/Prefabs/UnobtaniumBatteryModelData.cs
+++ UnobtaniumBatteries/Prefabs/UnobtaniumBatteryModelData.cs
@@ -8,9 +8,9 @@ namespace UnobtaniumBatteries.Prefabs
     internal class UnobtaniumBatteryModelData: CBModelData
     {
         public override Texture2D CustomTexture => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "battery_skin.png"));
-        public override Texture2D CustomNormalMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "battery_skin_normal.png"));
-        public override Texture2D CustomSpecMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "battery_skin_spec.png"));
-        public override Texture2D CustomIllumMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "battery_skin_illum.png"));
+        public override Texture2D CustomNormalMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "battery_normal.png"));
+        public override Texture2D CustomSpecMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "battery_spec.png"));
+        public override Texture2D CustomIllumMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "battery_illum.png"));
 
         public override float CustomIllumStrength => 1f;
 
--- UnobtaniumBatteries/Prefabs/UnobtaniumPowerCellModelData.cs
+++ UnobtaniumBatteries/Prefabs/UnobtaniumPowerCellModelData.cs
@@ -8,9 +8,9 @@ namespace UnobtaniumBatteries.Prefabs
     internal class UnobtaniumPowerCellModelData : CBModelData
     {
         public override Texture2D CustomTexture => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "cell_skin.png"));
-        public override Texture2D CustomNormalMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "cell_skin_normal.png"));
-        public override Texture2D CustomSpecMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "cell_skin_spec.png"));
-        public override Texture2D CustomIllumMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "cell_skin_illum.png"));
+        public override Texture2D CustomNormalMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "cell_normal.png"));
+        public override Texture2D CustomSpecMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "cell_spec.png"));
+        public override Texture2D CustomIllumMap => ImageUtils.LoadTextureFromFile(Path.Combine(Main.AssetsFolder, "cell_illum.png"));
 
         public override float CustomIllumStrength => 1f;
 
