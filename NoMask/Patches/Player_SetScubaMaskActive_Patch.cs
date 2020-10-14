2a472b77faab30085723d0ff9665a7fd19e6e76d
MrPurple6411 <mrpurple6411@gmail.com>
2020-10-14 09:33:00 -0600

    NoMask 1.0

---
+++ NoMask/Patches/Player_SetScubaMaskActive_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoMask.Patches
+{
+    [HarmonyPatch(typeof(Player), nameof(Player.SetScubaMaskActive))]
+    public static class Player_SetScubaMaskActive_Patch
+    {
+        [HarmonyPrefix]
+        public static void Prefix(ref bool state)
+        {
+            state = false;
+        }
+    }
+}
