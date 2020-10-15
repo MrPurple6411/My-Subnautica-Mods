3cab0728878edef2d372bed07a2f642711a6df8e
MrPurple6411 <mrpurple6411@gmail.com>
2020-10-15 06:15:47 -0600

    Fix for pickupable null ref when creatures breed.

--- CustomCommands/Main.cs
+++ CustomCommands/Main.cs
@@ -16,31 +16,31 @@ namespace CustomCommands
 #if SN1
 		internal static Dictionary<TechType, WaterParkCreatureParameters> CreatureParameters = new Dictionary<TechType, WaterParkCreatureParameters>()
 		{
-			{ TechType.ReaperLeviathan, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, true) },
-			{ TechType.SeaDragon, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, true) },
-			{ TechType.GhostLeviathan, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, true) },
-			{ TechType.SeaEmperorJuvenile, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, true) },
-			{ TechType.SeaEmperorBaby, new WaterParkCreatureParameters(0.1f, 0.3f, 1f, 3f, true) },
-			{ TechType.GhostLeviathanJuvenile, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, true) },
-			{ TechType.Warper, new WaterParkCreatureParameters(0.05f, 0.2f, 1f, 3f, true) },
+			{ TechType.ReaperLeviathan, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, false) },
+			{ TechType.SeaDragon, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, false) },
+			{ TechType.GhostLeviathan, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, false) },
+			{ TechType.SeaEmperorJuvenile, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, false) },
+			{ TechType.SeaEmperorBaby, new WaterParkCreatureParameters(0.1f, 0.3f, 1f, 3f, false) },
+			{ TechType.GhostLeviathanJuvenile, new WaterParkCreatureParameters(0.01f, 0.05f, 1f, 3f, false) },
+			{ TechType.Warper, new WaterParkCreatureParameters(0.05f, 0.2f, 1f, 3f, false) },
 		};
 #elif BZ
 		internal static Dictionary<TechType, WaterParkCreatureData> CreatureParameters = new Dictionary<TechType, WaterParkCreatureData>()
 		{
 			{ TechType.Chelicerate, new WaterParkCreatureData()
-			{ initialSize = 0.01f, maxSize = 0.05f, outsideSize = 1f, growingPeriodInDays = 3f, isPickupableOutside = true,
+			{ initialSize = 0.01f, maxSize = 0.05f, outsideSize = 1f, growingPeriodInDays = 3f, isPickupableOutside = false,
 				adultPrefab = CraftData.GetPrefabForTechType(TechType.Chelicerate), canBreed = true, eggOrChildPrefab = CraftData.GetPrefabForTechType(TechType.Chelicerate)  } },
 			{ TechType.ShadowLeviathan, new WaterParkCreatureData()
-			{ initialSize = 0.01f, maxSize = 0.05f, outsideSize = 1f, growingPeriodInDays =3f, isPickupableOutside = true,
+			{ initialSize = 0.01f, maxSize = 0.05f, outsideSize = 1f, growingPeriodInDays =3f, isPickupableOutside = false,
 				adultPrefab = CraftData.GetPrefabForTechType(TechType.ShadowLeviathan), canBreed = true, eggOrChildPrefab = CraftData.GetPrefabForTechType(TechType.ShadowLeviathan) } },
 			{ TechType.IceWorm, new WaterParkCreatureData()
-			{ initialSize = 0.01f, maxSize = 0.05f, outsideSize = 1f, growingPeriodInDays =3f, isPickupableOutside = true,
+			{ initialSize = 0.01f, maxSize = 0.05f, outsideSize = 1f, growingPeriodInDays =3f, isPickupableOutside = false,
 				adultPrefab = CraftData.GetPrefabForTechType(TechType.IceWorm), canBreed = true, eggOrChildPrefab = CraftData.GetPrefabForTechType(TechType.IceWorm) } },
 			{ TechType.SeaEmperorJuvenile, new WaterParkCreatureData()
-			{ initialSize = 0.01f, maxSize = 0.05f, outsideSize = 1f, growingPeriodInDays =3f, isPickupableOutside = true,
+			{ initialSize = 0.01f, maxSize = 0.05f, outsideSize = 1f, growingPeriodInDays =3f, isPickupableOutside = false,
 				adultPrefab = CraftData.GetPrefabForTechType(TechType.SeaEmperorJuvenile), canBreed = true, eggOrChildPrefab = CraftData.GetPrefabForTechType(TechType.SeaEmperorJuvenile) } },
 			{ TechType.Warper, new WaterParkCreatureData()
-			{ initialSize = 0.05f, maxSize = 0.2f, outsideSize = 1f, growingPeriodInDays =3f, isPickupableOutside = true,
+			{ initialSize = 0.05f, maxSize = 0.2f, outsideSize = 1f, growingPeriodInDays =3f, isPickupableOutside = false,
 				adultPrefab = CraftData.GetPrefabForTechType(TechType.Warper), canBreed = true, eggOrChildPrefab = CraftData.GetPrefabForTechType(TechType.Warper) } },
 		};
 #endif
