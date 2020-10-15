fa2089043caa61dbf7ea369a5b8ca88773add510
MrPurple6411 <mrpurple6411@gmail.com>
2020-10-15 06:15:13 -0600

    Seatruck BM

---
+++ BuilderModule-SeaTruck/Main.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+using BuilderModule_SeaTruck.Configuration;
+using SMLHelper.V2.Handlers;
+using System.Reflection;
+
+namespace BuilderModule_SeaTruck
+{
+    [QModCore]
+    public static class Main
+    {
+        internal static Assembly assembly = Assembly.GetExecutingAssembly();
+        internal static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+
+        [QModPatch]
+        public static void Load()
+        {
+            Harmony.CreateAndPatchAll(assembly, $"MrPurple6411_{assembly.GetName().Name}");
+        }
+    }
+}
