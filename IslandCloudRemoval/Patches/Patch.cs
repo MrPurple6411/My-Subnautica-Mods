2fd2e8840e2335498e9f3833b72cd6133428bb88
MrPurple6411 <mrpurple6411@gmail.com>
2020-12-21 13:49:50 -0700

    Island Cloud Removal

---
+++ IslandCloudRemoval/Patches/Patch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using System.Collections;
+using UnityEngine;
+using UWE;
+
+namespace IslandCloudRemoval.Patches
+{
+    [HarmonyPatch(typeof(Player), nameof(Player.Awake))]
+    public static class Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            CoroutineHost.StartCoroutine(NewMethod());
+        }
+
+        private static IEnumerator NewMethod()
+        {
+            GameObject x_Clouds = GameObject.Find("x_Clouds(Clone)");
+
+            yield return new WaitUntil(() => (x_Clouds = x_Clouds ?? GameObject.Find("x_Clouds(Clone)")) != null);
+
+            MeshRenderer[] renderers = x_Clouds.GetComponentsInChildren<MeshRenderer>();
+
+            foreach (MeshRenderer renderer in renderers)
+            {
+                if (renderer.name.Contains("x_IslandClouds"))
+                    renderer.gameObject.SetActive(false);
+            }
+
+            yield break;
+        }
+    }
+}
