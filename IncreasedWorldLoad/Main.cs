ccc4d9d3eea5d53e820dc9ddec47e5d4dcd25688
MrPurple6411 <mrpurple6411@gmail.com>
2020-12-21 13:49:24 -0700

    Icreased World Load

---
+++ IncreasedWorldLoad/Main.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+using System.Reflection;
+
+namespace IncreasedWorldLoad
+{
+    [QModCore]
+    public static class Main
+    {
+        internal static Assembly assembly = Assembly.GetExecutingAssembly();
+
+        [QModPatch]
+        public static void Load()
+        {
+            Harmony.CreateAndPatchAll(assembly, $"MrPurple6411_{assembly.GetName().Name}");
+        }
+    }
+}
