79b952ab5d3559afabc7ac8892f57be43c245a9c
MrPurple6411 <mrpurple6411@gmail.com>
2021-06-08 18:57:15 -0600

    NML

---
+++ NoMagicLights/Main.cs
@@ -0,0 +1,17 @@
+﻿namespace NoMagicLights
+{
+    using HarmonyLib;
+    using QModManager.API.ModLoading;
+    using System.Reflection;
+
+    [QModCore]
+    public class Main
+    {
+        [QModPatch]
+        public static void Load()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            new Harmony($"MrPurple6411_{assembly.GetName().Name}").PatchAll(assembly);
+        }
+    }
+}
