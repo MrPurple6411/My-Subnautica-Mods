12e1d75cacfe660ce8837b6792b1b6a63ecd3d31
MrPurple6411 <mrpurple6411@gmail.com>
2022-12-21 22:53:40 -0700

    Small bugfixes for spawning
    Required the InstantiateAsync patch in the new smlhelper to work for modded techtypes also

--- IncreasedChunkDrops/Main.cs
+++ IncreasedChunkDrops/Main.cs
@@ -5,7 +5,8 @@
     using SMLHelper.V2.Handlers;
     using System.Reflection;
     using BepInEx;
-    
+    using BepInEx.Logging;
+
     [BepInPlugin(GUID, MODNAME, VERSION)]
     public class Main: BaseUnityPlugin
     {
@@ -18,11 +19,13 @@
             AUTHOR = "MrPurple6411",
             GUID = AUTHOR + "_" + MODNAME,
             VERSION = "1.0.0.0";
+        internal static ManualLogSource logSource;
 
         #endregion
 
         private void Awake()
         {
+            logSource = Logger;
             var assembly = Assembly.GetExecutingAssembly();
             new Harmony($"MrPurple6411_{assembly.GetName().Name}").PatchAll(assembly);
         }
--- IncreasedChunkDrops/Patches/BreakableResource_BreakIntoResources_Patch.cs
+++ IncreasedChunkDrops/Patches/BreakableResource_BreakIntoResources_Patch.cs
@@ -4,6 +4,9 @@
     using UnityEngine;
     using Random = UnityEngine.Random;
     using UnityEngine.AddressableAssets;
+    using BepInEx.Logging;
+    using UWE;
+    using System.Collections;
 
     [HarmonyPatch(typeof(BreakableResource), nameof(BreakableResource.BreakIntoResources))]
     internal class BreakableResource_BreakIntoResources_Patch
@@ -18,41 +21,64 @@
             {
                 AssetReferenceGameObject assetReferenceGameObject = null;
                 var flag = false;
-                for (var i = 0; i < __instance.numChances; i++)
+                for(var i = 0; i < __instance.numChances; i++)
                 {
                     assetReferenceGameObject = __instance.ChooseRandomResource();
-                    if (assetReferenceGameObject != null)
+                    if(assetReferenceGameObject != null)
                     {
                         extraSpawns--;
                         flag = true;
                         break;
                     }
                 }
-                if (!flag)
+                if(!flag)
                 {
                     assetReferenceGameObject = __instance.defaultPrefabReference;
                     extraSpawns--;
                 }
 
-                if (assetReferenceGameObject == null) continue;
-                var loadPrefab = Addressables.LoadAssetAsync<GameObject>(assetReferenceGameObject.RuntimeKey as string);
-                loadPrefab.Completed += (prefabTask) => {
+                if(assetReferenceGameObject == null)
+                    continue;
 
-                    var prefab = prefabTask.Result;
-                    if (prefab is null)
-                        return;
-
-                    var rigidbody = Object.Instantiate(prefab, position, Quaternion.identity).GetComponent<Rigidbody>();
-
-                    if (rigidbody == null) return;
-                    rigidbody.isKinematic = false;
-                    rigidbody.maxDepenetrationVelocity = 0.5f;
-                    rigidbody.maxAngularVelocity = 1f;
-                    rigidbody.AddTorque(Vector3.right * Random.Range(6f, 12f));
-                    rigidbody.AddForce(position * 0.2f);
-
-                };
+                CoroutineHost.StartCoroutine(SpawnObject(assetReferenceGameObject, position));
             }
+        }
+
+        private static IEnumerator SpawnObject(AssetReferenceGameObject assetReferenceGameObject, Vector3 position)
+        {
+            var log = assetReferenceGameObject.RuntimeKey is string key && !key.EndsWith("prefab");
+            var task = AddressablesUtility.InstantiateAsync(assetReferenceGameObject.RuntimeKey as string, null, position, Quaternion.identity);
+            yield return task;
+            var go = task.GetResult();
+
+            if(log)
+            {
+                Main.logSource.LogMessage($"key: {assetReferenceGameObject.RuntimeKey}");
+                Main.logSource.LogMessage($"go: {go != null}");
+            }
+
+            if(go is null)
+                yield break;
+
+            var rigidbody = go.GetComponent<Rigidbody>();
+            if(log)
+            {
+                Main.logSource.LogMessage($"rigidbody: {rigidbody != null}");
+            }
+
+            if(rigidbody == null)
+                yield break;
+
+            if(log)
+            {
+                Main.logSource.LogMessage($"Distance: {Vector3.Distance(Player.main.transform.position, go.transform.position)}");
+            }
+
+            rigidbody.isKinematic = false;
+            rigidbody.maxDepenetrationVelocity = 0.5f;
+            rigidbody.maxAngularVelocity = 1f;
+            rigidbody.AddTorque(Vector3.right * Random.Range(6f, 12f));
+            rigidbody.AddForce(position * 0.2f);
         }
     }
 }
