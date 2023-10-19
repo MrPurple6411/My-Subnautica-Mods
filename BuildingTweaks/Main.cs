42b2e37d2ed4d99a026ae26f3ffce63001f58107
MrPurple6411 <mrpurple6411@gmail.com>
2023-10-18 21:45:57 -0600

    Fix mistaken Soft Dependency

--- BuildingTweaks/Main.cs
+++ BuildingTweaks/Main.cs
@@ -7,7 +7,7 @@ using System.Reflection;using BepInEx;
 using BepInEx.Logging;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-[BepInDependency("com.snmodding.nautilus", BepInDependency.DependencyFlags.SoftDependency)]
+[BepInDependency("com.snmodding.nautilus", BepInDependency.DependencyFlags.HardDependency)]
 public class Main: BaseUnityPlugin
 {
     public static SMLConfig SMLConfig { get; } = OptionsPanelHandler.RegisterModOptions<SMLConfig>();
