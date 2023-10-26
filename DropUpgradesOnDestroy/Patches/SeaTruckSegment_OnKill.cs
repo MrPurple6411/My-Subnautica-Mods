4e8db9aaf762f763b3b4fbb1c1e571bc306a9e37
MrPurple6411 <mrpurple6411@gmail.com>
2023-10-26 16:25:18 -0600

    Update SeaTruckSegment_OnKill.cs

--- DropUpgradesOnDestroy/Patches/SeaTruckSegment_OnKill.cs
+++ DropUpgradesOnDestroy/Patches/SeaTruckSegment_OnKill.cs
@@ -1,28 +1,24 @@
-﻿#if BELOWZERO
-namespace DropUpgradesOnDestroy.Patches
+#if BELOWZERO
+namespace DropUpgradesOnDestroy.Patches;
+
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+
+[HarmonyPatch(typeof(SeaTruckSegment), nameof(SeaTruckSegment.OnKill))]
+public class SeaTruckSegment_OnKill
 {
-    using HarmonyLib;
-    using System.Collections.Generic;
-    using System.Linq;
-#if SUBNAUTICA
-    using UnityEngine;
-#endif
+	private static SeaTruckSegment _lastDestroyed;
 
-    [HarmonyPatch(typeof(SeaTruckSegment), nameof(SeaTruckSegment.OnKill))]
-    public class SeaTruckSegment_OnKill
-    {
-        private static SeaTruckSegment lastDestroyed;
+	[HarmonyPrefix]
+	public static void Prefix(SeaTruckSegment __instance)
+	{
+		if (!__instance.IsFront() || __instance == _lastDestroyed) return;
+			_lastDestroyed = __instance;
+		var equipment = __instance.motor?.upgrades?.modules?.equipment?.Values.Where((e) => e != null).ToList() ?? new List<InventoryItem>();
 
-        [HarmonyPrefix]
-        public static void Prefix(SeaTruckSegment __instance)
-        {
-            if (!__instance.IsFront() || __instance == lastDestroyed) return;
-            lastDestroyed = __instance;
-            var equipment = __instance.motor?.upgrades?.modules?.equipment?.Values.Where((e) => e != null).ToList() ?? new List<InventoryItem>();
-
-            var position = __instance.gameObject.transform.position;
-            Main.SpawnModuleNearby(equipment, position);
-        }
-    }
+		var position = __instance.gameObject.transform.position;
+		Main.SpawnModuleNearby(equipment, position);
+	}
 }
 #endif
