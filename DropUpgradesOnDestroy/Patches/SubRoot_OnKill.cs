f72e59832f3f67bcc699ca837b2f7067c3aca721
MrPurple6411 <mrpurple6411@gmail.com>
2023-10-26 16:25:46 -0600

    Simplified MCU code.

--- DropUpgradesOnDestroy/Patches/SubRoot_OnKill.cs
+++ DropUpgradesOnDestroy/Patches/SubRoot_OnKill.cs
@@ -1,9 +1,6 @@
-﻿namespace DropUpgradesOnDestroy.Patches;
+namespace DropUpgradesOnDestroy.Patches;
 
 using HarmonyLib;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 
 [HarmonyPatch(typeof(SubRoot), nameof(SubRoot.OnKill))]
@@ -12,36 +9,11 @@ public class SubRoot_OnKill
     [HarmonyPrefix]
     public static void Prefix(SubRoot __instance)
     {
-        if(__instance.upgradeConsole == null || __instance.upgradeConsole.modules.equipment?.Values is null) return;
-        
-        var equipment = __instance.upgradeConsole.modules.equipment.Values.Where((e) => e != null).ToList();
+		var equipment = (from UpgradeConsole console in __instance.GetComponentsInChildren<UpgradeConsole>(true)
+										 from InventoryItem item in console?.modules?.equipment?.Values?.Where((e) => e != null)
+										 select item).ToList();
 
-        var MCU = AppDomain.CurrentDomain.GetAssemblies()
-                                                    .Where((x) => x.FullName.StartsWith("MoreCyclopsUpgrades"))
-                                                    .FirstOrFallback(null);
-
-        if(MCU != null)
-        {
-            var IMCUCrossMod = AccessTools.TypeByName("MoreCyclopsUpgrades.API.IMCUCrossMod");
-            var MCUServices = AccessTools.TypeByName("MoreCyclopsUpgrades.API.MCUServices");
-            var UpgradeSlot = AccessTools.TypeByName("MoreCyclopsUpgrades.API.Buildables.UpgradeSlot");
-
-            if(IMCUCrossMod != null && UpgradeSlot != null)
-            {
-                var IMCUCrossMod_GetAllUpgradeSlots = AccessTools.Method(IMCUCrossMod, "GetAllUpgradeSlots");
-                var UpgradeSlot_GetItemInSlot = AccessTools.Method(UpgradeSlot, "GetItemInSlot");
-                var CrossMod = AccessTools.Property(MCUServices, "CrossMod");
-                if(IMCUCrossMod_GetAllUpgradeSlots != null && UpgradeSlot_GetItemInSlot != null && CrossMod != null)
-                {
-                    var listType = typeof(List<>).MakeGenericType(UpgradeSlot);
-                    var upgradeSlots = (IList)Activator.CreateInstance(listType, IMCUCrossMod_GetAllUpgradeSlots.Invoke(CrossMod.GetValue(null), new object[] { __instance }));
-
-                    equipment.AddRange(from object slot in upgradeSlots select (InventoryItem) UpgradeSlot_GetItemInSlot.Invoke(slot, null) into item where item != null select item);
-                }
-            }
-        }
-
-        var position = __instance.gameObject.transform.position;
+		var position = __instance.gameObject.transform.position;
         Main.SpawnModuleNearby(equipment, position);
     }
 }
