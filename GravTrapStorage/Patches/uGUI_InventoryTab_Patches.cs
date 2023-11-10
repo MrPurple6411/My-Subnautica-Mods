3796102397010945ac995bdf47a4c89745e75f9a
MrPurple6411 <mrpurple6411@gmail.com>
2023-11-10 11:25:54 -0700

    Fix for pda closing but not opening new storage tab

--- GravTrapStorage/Patches/uGUI_InventoryTab_Patches.cs
+++ GravTrapStorage/Patches/uGUI_InventoryTab_Patches.cs
@@ -1,83 +1,85 @@
-﻿using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
-
 namespace GravTrapStorage.Patches;
 
 using HarmonyLib;
+using System.Collections;
+using System.Text;
+using UnityEngine;
 
 [HarmonyPatch]
 internal class UGUIInventoryTabPatches
 {
-    public static InventoryItem CurrentItem { get; set; }
+	public static InventoryItem CurrentItem { get; set; }
 
-    [HarmonyPatch(typeof(uGUI_InventoryTab), nameof(uGUI_InventoryTab.OnPointerEnter))]
-    [HarmonyPostfix]
-    public static void OnPointerEnter_Postfix(InventoryItem item)
-    {
-        CurrentItem = item;
-    }
+	[HarmonyPatch(typeof(uGUI_InventoryTab), nameof(uGUI_InventoryTab.OnPointerEnter))]
+	[HarmonyPostfix]
+	public static void OnPointerEnter_Postfix(InventoryItem item)
+	{
+		CurrentItem = item;
+	}
 
-    [HarmonyPatch(typeof(uGUI_InventoryTab), nameof(uGUI_InventoryTab.OnPointerExit))]
-    [HarmonyPostfix]
-    public static void OnPointerExit_Postfix()
-    {
-        CurrentItem = null;
-    }
+	[HarmonyPatch(typeof(uGUI_InventoryTab), nameof(uGUI_InventoryTab.OnPointerExit))]
+	[HarmonyPostfix]
+	public static void OnPointerExit_Postfix()
+	{
+		CurrentItem = null;
+	}
 
-    [HarmonyPatch(typeof(Player), nameof(Player.Update))]
-    [HarmonyPostfix]
-    public static void Update_Postfix()
-    {
-        PDA pda = Player.main.GetPDA();
-        if (!pda.isInUse) return;
-        bool usingController = GameInput.GetPrimaryDevice() == GameInput.Device.Controller;
-        if (!GameInput.GetButtonDown(usingController ? GameInput.Button.Sprint : GameInput.Button.AltTool)) return;
-        Pickupable pickupable;
-        if (CurrentItem != null)
-        {
-            pickupable = CurrentItem.item;
-        }
-        else
-        {
-            GamepadInputModule inputModule = GamepadInputModule.current;
-            if (inputModule.currentNavigableGrid?.GetSelectedItem() is not uGUI_ItemIcon
-                {
-                    manager: uGUI_ItemsContainer itemsContainer
-                } itemIcon) return;
-            if (!itemsContainer.icons.TryGetValue(itemIcon, out InventoryItem inventoryItem)) return;
+	[HarmonyPatch(typeof(Player), nameof(Player.Update))]
+	[HarmonyPostfix]
+	public static void Update_Postfix(Player __instance)
+	{
+		PDA pda = Player.main.GetPDA();
+		if (!pda.isInUse) return;
+		bool usingController = GameInput.GetPrimaryDevice() == GameInput.Device.Controller;
+		if (!GameInput.GetButtonDown(usingController ? GameInput.Button.Sprint : GameInput.Button.AltTool)) return;
+		Pickupable pickupable;
+		if (CurrentItem != null)
+		{
+			pickupable = CurrentItem.item;
+		}
+		else
+		{
+			GamepadInputModule inputModule = GamepadInputModule.current;
+			if (inputModule.currentNavigableGrid?.GetSelectedItem() is not uGUI_ItemIcon { manager: uGUI_ItemsContainer itemsContainer } itemIcon) return;
+			if (!itemsContainer.icons.TryGetValue(itemIcon, out InventoryItem inventoryItem)) return;
 
-            pickupable = inventoryItem.item;
-        }
+			pickupable = inventoryItem.item;
+		}
 
-        StorageContainer storageContainer = pickupable.gameObject.GetComponentInChildren<StorageContainer>(true);
-        if (storageContainer == null) return;
+		StorageContainer storageContainer = pickupable.gameObject.GetComponentInChildren<StorageContainer>(true);
+		if (storageContainer == null) return;
 
-        if (!storageContainer.GetOpen())
-        {
-            pda.Close();
-            if (pickupable.gameObject.TryGetComponent(out Gravsphere gravsphere))
-                storageContainer.container.SetAllowedTechTypes(new[] {TechType.None});
-            storageContainer.Open(storageContainer.transform);
-        }
-        else
-        {
-            pda.Close();
-            pda.Open(PDATab.Inventory);
-        }
-    }
+		__instance.StartCoroutine(OpenStorage(pda, pickupable, storageContainer));
+	}
 
+	private static IEnumerator OpenStorage(PDA pda, Pickupable pickupable, StorageContainer storageContainer)
+	{
+		if (!storageContainer.GetOpen())
+		{
+			pda.Close();
+			if (pickupable.gameObject.TryGetComponent(out Gravsphere gravsphere))
+				storageContainer.container.SetAllowedTechTypes(new[] { TechType.None });
+			storageContainer.Open(storageContainer.transform);
+			Inventory.main.SetUsedStorage(storageContainer.container, false);
+			yield return new WaitUntil(() => pda.Open(PDATab.Inventory, storageContainer.transform, new PDA.OnClose(storageContainer.OnClosePDA)));
+		}
+		else
+		{
+			pda.Close();
+			yield return new WaitUntil(()=> pda.Open(PDATab.Inventory));
+		}
+	}
 
-    [HarmonyPatch(typeof(TooltipFactory), nameof(TooltipFactory.ItemActions))]
-    [HarmonyPostfix]
-    public static void ItemActions_Postfix(StringBuilder sb, InventoryItem item)
-    {
-        StorageContainer storageContainer = item.item.gameObject.GetComponentInChildren<StorageContainer>(true);
-        if (storageContainer == null) return;
+	[HarmonyPatch(typeof(TooltipFactory), nameof(TooltipFactory.ItemActions))]
+	[HarmonyPostfix]
+	public static void ItemActions_Postfix(StringBuilder sb, InventoryItem item)
+	{
+		StorageContainer storageContainer = item.item.gameObject.GetComponentInChildren<StorageContainer>(true);
+		if (storageContainer == null) return;
 
-        string msg = storageContainer.GetOpen() ? "Close Storage" : "Open Storage";
-        bool usingController = GameInput.GetPrimaryDevice() == GameInput.Device.Controller;
-        TooltipFactory.WriteAction(sb,
-            uGUI.FormatButton(usingController ? GameInput.Button.Sprint : GameInput.Button.AltTool), msg);
-    }
+		string msg = storageContainer.GetOpen() ? "Close Storage" : "Open Storage";
+		bool usingController = GameInput.GetPrimaryDevice() == GameInput.Device.Controller;
+		TooltipFactory.WriteAction(sb,
+			uGUI.FormatButton(usingController ? GameInput.Button.Sprint : GameInput.Button.AltTool), msg);
+	}
 }
