1688b1d5a9a860d85b669582531bef895b67050e
Joshua Gibbs <mrpurple6411@gmail.com>
2025-02-22 15:01:47 -0700

    Update ClearerWaterparkGlass.cs

--- BetterACU/Patches/ClearerWaterparkGlass.cs
+++ BetterACU/Patches/ClearerWaterparkGlass.cs
@@ -1,4 +1,4 @@
-﻿namespace BetterACU.Patches;
+namespace BetterACU.Patches;
 
 using System.Collections.Generic;
 using System.Linq;
@@ -8,25 +8,23 @@ using UnityEngine;
 [HarmonyPatch]
 public static class ClearerWaterparkGlass
 {
-    private static readonly int Color1 = Shader.PropertyToID("_Color");
-    private static readonly int Fresnel = Shader.PropertyToID("_Fresnel");
-    private static readonly int Shininess = Shader.PropertyToID("_Shininess");
-    private static readonly int SpecInt = Shader.PropertyToID("_SpecInt");
+    private static readonly int _color1 = Shader.PropertyToID("_Color");
+    private static readonly int _fresnel = Shader.PropertyToID("_Fresnel");
+    private static readonly int _shininess = Shader.PropertyToID("_Shininess");
+    private static readonly int _specInt = Shader.PropertyToID("_SpecInt");
 
     [HarmonyPatch(typeof(Base), nameof(Base.RebuildGeometry))]
     [HarmonyPostfix]
     public static void Postfix(Base __instance)
     {
         var waterParkGeometries = __instance.gameObject.GetComponentsInChildren<Renderer>()?.ToList() ?? new List<Renderer>();
-        foreach (var material in waterParkGeometries.Select(largeRoomWaterPark => largeRoomWaterPark != null
-                ? largeRoomWaterPark.material
-                : null)
+        foreach (var material in waterParkGeometries.Select(largeRoomWaterPark => largeRoomWaterPark?.material)
             .Where(material => material != null && material.name.StartsWith("Large_Aquarium_Room_generic_glass")))
         {
-            material.SetColor(Color1, new Color(0f, 0.609009f, 0.8088291f, 0.15f));
-            material.SetFloat(Fresnel, 0.9f);
-            material.SetFloat(Shininess, 6f);
-            material.SetFloat(SpecInt, 5f);
+            material.SetColor(_color1, new Color(0f, 0.609009f, 0.8088291f, 0.15f));
+            material.SetFloat(_fresnel, 0.9f);
+            material.SetFloat(_shininess, 6f);
+            material.SetFloat(_specInt, 5f);
         }
     }
 }
